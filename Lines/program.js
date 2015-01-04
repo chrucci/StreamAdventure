956a7a918b0687a48ede51d5065686917b33b638
Chris Rucci <chrucci_github@spamex.com>
2015-01-04 14:46:06 -0500

    Finished Lesson 5

--- Lines/program.js
+++ Lines/program.js
@@ -1,3 +1,25 @@
+
 /**
  * Created by Chris on 1/4/2015.
  */
+
+var thru = require('through'),
+    split = require('split'),
+    lineIndex = 0,
+    isEvenLine = function() {
+        return lineIndex % 2 === 0;
+    },
+    write = function(dataBuf){
+        var output = dataBuf.toString();
+        lineIndex++;
+        if (isEvenLine())
+            output = output.toUpperCase();
+        else
+            output = output.toLocaleLowerCase();
+        this.queue(output + '\n');
+    };
+
+process.stdin
+    .pipe(split())
+    .pipe(thru(write))
+    .pipe(process.stdout);
