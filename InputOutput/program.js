2271cfffa4f5db17b282163958f960e278fc6b32
Chris Rucci <chrucci_github@spamex.com>
2015-01-04 13:28:21 -0500

    Lesson 3
    adding the lesson 3 code

---
+++ InputOutput/program.js
@@ -0,0 +1,4 @@
+/**
+ * Created by Chris on 1/4/2015.
+ */
+process.stdin.pipe(process.stdout);
--- MeetPipe/program.js
+++ MeetPipe/program.js
@@ -1,3 +1,9 @@
 /**
  * Created by Chris on 1/4/2015.
  */
+
+var fs = require('fs'),
+    filepath = process.argv[2];
+
+    fs.createReadStream(filepath).pipe(process.stdout);
+
